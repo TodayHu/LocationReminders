ae8390f822874726c17c2baf2d7c714fb528a4df
Anne <star7anne@yahoo.com>
2015-02-02 09:35:00 -0800

    Initial Commit

---
+++ LocationReminders/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocationReminders
+//
+//  Created by Annemarie Ketola on 2/2/15.
+//  Copyright (c) 2015 Up Early, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
